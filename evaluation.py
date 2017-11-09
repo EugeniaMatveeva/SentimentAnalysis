8e04586228c0d3d457d56952a8c83f53c8e2b0b8
EugeniaMatveeva <eugenie87@gmail.com>
2017-11-09 13:17:14 +0300

    added saving/loading document-term transformation matrix for reusing with different classifiers; added grid search.

--- evaluation.py
+++ evaluation.py
@@ -8,7 +8,7 @@ from sklearn.pipeline import make_union, make_pipeline
 from sklearn.preprocessing import LabelEncoder, Normalizer
 
 from helper import *
-from parameter_tuning import evaluate
+from parameter_tuning import evaluate, grid_search
 from transformations.nltk_preprocessor import NLTKPreprocessor
 from transformations.inquirerlex_transform import InquirerLexTransform
 
@@ -16,54 +16,55 @@ now = datetime.datetime.now()
 
 class Evaluator:
 
-    def __init__(self, preprocessor=None, vectorizer=None, use_test_split=True, use_inqlex=False, save_paths=None, load_paths=None):
-        if load_paths and 'preprocessor' in load_paths:
-            self.preprocessor = self.load_step(save_paths['preprocessor'])
-            self.preprocessor_is_fit = True
+    def __init__(self, preprocessor=None, vectorizer=None, use_inqlex=False, load_path=None):
+        if load_path is not None:
+            self.load_data_transformation(load_path)
+            self.model_is_fit = True
         else:
+            self.model_is_fit = False
             self.preprocessor = preprocessor or NLTKPreprocessor()
-        if load_paths and 'vectorizer' in load_paths:
-            self.vectorizer = self.load_step(save_paths['vectorizer'])
-            self.vectorizer_is_fit = True
-        else:
             self.vectorizer = vectorizer or TfidfVectorizer(tokenizer=identity, preprocessor=None, lowercase=False, ngram_range=(1,2), min_df=2)
-            self.vectorizer_is_fit = False
-        self.use_test_split = use_test_split
         self.use_lex = use_inqlex
-        self.save_paths = save_paths
 
     def get_step(self, step_name):
         return self.model.named_steps[filter(lambda nm: step_name in nm, self.model.named_steps)[0]]
 
-    def test_classifier(self, classifier, classifier_name, X, y, show_features=False, show_errors=False):
+    def prepare_data_transformation(self, X, y, save_path=None):
+        X_prep = self.preprocessor.fit_transform(X, y)
+        X_vect = self.vectorizer.fit_transform(X_prep, y)
+        self.save(X_vect, save_path + '_dt_matrix')
+        self.save(self.preprocessor, save_path + '_preprocessor')
+        self.save(self.vectorizer, save_path + '_vectorizer')
+
+    def load_data_transformation(self, load_path):
+        self.preprocessor = self.load(load_path + '_preprocessor')
+        self.vectorizer = self.load(load_path + '_vectorizer')
+        self.dt_matrix = self.load(load_path + '_dt_matrix')
+
+    def test_classifier(self, classifier, classifier_name, X, y, test_split=0, show_features=False, show_errors=False):
         # Label encode the targets
         self.labels = LabelEncoder()
         y = self.labels.fit_transform(y)
 
         print("Evaluation model fit")
-        if self.use_test_split:
-            X_train, X_test, y_train, y_test = tts(X, y, test_size=0.2)
+        if test_split > 0:
+            X_train, X_test, y_train, y_test = tts(X, y, test_size=test_split, random_state=42)
             print("Building for evaluation")
-            self.build_classifier(classifier, classifier_name, X_train, y_train)
+            self.build_model(classifier, classifier_name)
+            self.fit_model(X_train, y_train)
             y_pred, score = self.evaluate_on_test_split(X_test, y_test)
+
             if show_features and hasattr(classifier, 'coef_'):
                 print(self.show_model_output())
-
             if show_errors:
                 self.show_error_predictions(y_pred, X_test, y_test)
         else:
+            self.build_model(classifier, classifier_name)
             score = self.evaluate_on_cross_val(X, y, classifier, classifier_name)
         log_pipeline_results(self.model, score)
 
-        if self.save_paths:
-            for path in self.save_paths:
-                if 'preprocessor' in self.save_paths: self.save_step('preprocessor', self.save_paths['preprocessor'])
-                if 'vectorizer' in self.save_paths: self.save_step('vectorizer', self.save_paths['vectorizer'])
-                if 'model' in self.save_paths: self.save_step('model', self.save_paths['model'])
-
-
-    def build_classifier(self, classifier, classifier_name, X, y):
-        if (self.use_lex):
+    def build_model(self, classifier, classifier_name):
+        if self.use_lex:
             vectorizer_name = type(self.vectorizer).__name__ + '+InquirerLexTransform'
             lex_vectorizer = TfidfVectorizer(tokenizer=identity, preprocessor=None, lowercase=False)
             pipeline2 = make_pipeline(InquirerLexTransform(), lex_vectorizer)
@@ -81,15 +82,18 @@ class Evaluator:
                 ('vectorizer:' + type(self.vectorizer).__name__, self.vectorizer),
                 ('classifier:' + classifier_name or type(classifier).__name__, classifier),
             ])
-        self.model.fit(X, y)
+
+    def fit_model(self, X, y):
+        if self.model_is_fit:
+            self.get_step('classifier').fit(self.dt_matrix, y)
+        else:
+            self.model.fit(X, y)
         self.model.labels_ = self.labels
 
     def evaluate_on_cross_val(self, X, y, classifier, classifier_name=None):
-        self.build_classifier(classifier, classifier_name, X, y)
         score = evaluate(self.model, X, y, 3)
         print("Model mean accuracy: {:f}".format(score))
         return score
-
 
     def evaluate_on_test_split(self, X_test, y_test, verbose=True):
         if verbose:
@@ -100,13 +104,23 @@ class Evaluator:
         score = accuracy_score(y_test, y_pred)
         return y_pred, score
 
+    def choose_classifier_parameters(self, classifier, classifier_name, X, y, params_grid):
+        self.build_model(classifier, classifier_name)
+        res = grid_search(self.model, params_grid, X, y)
+        print ('Best params: ', res.best_params_)
+        print 'Best score: %f' % res.best_score_
+        return res
 
-    def save_step(self, step_name, path=None):
-        step = self.model.named_steps[filter(lambda nm: step_name in nm, self.model.named_steps)[0]]
-        with open(path or step_name, 'wb') as f:
-            pickle.dump(step, f)
 
-    def load_step(self, path):
+    def save_model(self, save_path, steps):
+        for s in steps:
+            self.save(self.get_step(s), save_path + '_' + s)
+
+    def save(self, obj, path):
+        with open(path, 'wb') as f:
+            pickle.dump(obj, f)
+
+    def load(self, path):
         with open(path, 'rb') as f:
             return pickle.load(f)
 
--- sa_main.py
+++ sa_main.py
@@ -1,6 +1,7 @@
 import random
 
 from nltk.corpus import movie_reviews
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import *
@@ -8,6 +9,7 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
+import pandas as pd
 
 from evaluation import *
 from parameter_tuning import evaluate
@@ -15,78 +17,56 @@ from transformations.nltk_preprocessor import NLTKPreprocessor
 
 now = datetime.datetime.now()
 
-
 def data_load(shuffle=True):
     X = [movie_reviews.raw(fileid) for fileid in movie_reviews.fileids()]
     y = [movie_reviews.categories(fileid)[0] for fileid in movie_reviews.fileids()]
     Xy = zip(X, y)
     if shuffle:
-        random.seed(42)
         random.shuffle(Xy, )
+    else:
+        random.shuffle(Xy, lambda: 0.42 )
     return [x[0] for x in Xy], [x[1] for x in Xy]
 
 def compare_classifiers(vectorizer, data, target, vectorizer_name=None):
     vectorizer_name = vectorizer_name or type(vectorizer).__name__
-    pipe_cnt_logreg = Pipeline([(vectorizer_name, vectorizer),
-                                ('logisticregression', LogisticRegression())])
-    log_pipeline_results(pipe_cnt_logreg, evaluate(pipe_cnt_logreg, data, target))
+    pipe_logreg= Pipeline([(vectorizer_name, vectorizer),
+                           ('logisticregression', LogisticRegression())])
+    log_pipeline_results(pipe_logreg, evaluate(pipe_logreg, data, target))
 
-    pipe_cnt_svc = Pipeline([(vectorizer_name, vectorizer),
-                              ('linearsvc', LinearSVC())])
-    log_pipeline_results(pipe_cnt_svc, evaluate(pipe_cnt_svc, data, target))
+    pipe_svc = Pipeline([(vectorizer_name, vectorizer),
+                         ('linearsvc', LinearSVC())])
+    log_pipeline_results(pipe_svc, evaluate(pipe_svc, data, target))
 
-    pipe_cnt_sgd = Pipeline([(vectorizer_name, vectorizer),
-                             ('sgdclassifier, loss=hinge, penalty=l2, alpha=1e-3', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42))])
-    log_pipeline_results(pipe_cnt_sgd, evaluate(pipe_cnt_sgd, data, target))
+    pipe_sgd = Pipeline([(vectorizer_name, vectorizer),
+                         ('sgdclassifier, loss=hinge, penalty=l2, alpha=1e-3', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42))])
+    log_pipeline_results(pipe_sgd, evaluate(pipe_sgd, data, target))
 
-    pipe_cnt_mlp = Pipeline([(vectorizer_name, vectorizer),
-                             ('mlp, (30,30,30)', MLPClassifier(hidden_layer_sizes=(30,30,30)))])
-    log_pipeline_results(pipe_cnt_mlp, evaluate(pipe_cnt_mlp, data, target))
+    pipe_mlp = Pipeline([(vectorizer_name, vectorizer),
+                         ('mlp, (30,30,30)', MLPClassifier(hidden_layer_sizes=(30,30,30)))])
+    log_pipeline_results(pipe_mlp, evaluate(pipe_mlp, data, target))
 
-    pipe_cnt_nb = Pipeline([(vectorizer_name, vectorizer),
-                            ('multinomialnb', MultinomialNB())])
-    log_pipeline_results(pipe_cnt_nb, evaluate(pipe_cnt_nb, data, target))
+    pipe_nb = Pipeline([(vectorizer_name, vectorizer),
+                        ('multinomialnb', MultinomialNB())])
+    log_pipeline_results(pipe_nb, evaluate(pipe_nb, data, target))
+
+    pipe_rf = Pipeline([(vectorizer_name, vectorizer),
+                        ('randomforest', RandomForestClassifier(n_jobs=-1, oob_score=True))])
+    log_pipeline_results(pipe_rf, evaluate(pipe_rf, data, target))
 
 
 X, y = data_load()
-# Label encode the targets
-labels = LabelEncoder()
-y = labels.fit_transform(y)
 
 cnt_vectorizer = CountVectorizer(tokenizer=identity, preprocessor=None, lowercase=False, ngram_range=(1,2), min_df=2)
-tf_vectorizer = TfidfVectorizer(tokenizer=identity, preprocessor=None, lowercase=False, ngram_range=(1,1), min_df=2)
-sgd_classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3)
-rfc = RandomForestClassifier(random_state=42, n_jobs=-1, oob_score=True)
+tf_vectorizer = TfidfVectorizer(tokenizer=identity, preprocessor=None, lowercase=False, ngram_range=(1,2), min_df=2)
 
-save_paths = {
-    'preprocessor': 'preprocessor',
-    'vectorizer': 'vectorizer'
-}
-load_paths = {
-    'preprocessor': 'preprocessor',
-    'vectorizer': 'vectorizer'
-}
-
-
-evaluator = Evaluator(preprocessor=NLTKPreprocessor(add_PosNeg=True), vectorizer=tf_vectorizer, use_inqlex=False, use_test_split=False)
-#test([X[0]], [y[0]])
-evaluator.test_classifier(sgd_classifier, 'sgd_classifier', X, y, show_errors=False)
-# evaluator.test_classifier(rfc, 'RandomForestClassifier', X, y, show_errors=False)
-# evaluator.test_classifier(MLPClassifier(hidden_layer_sizes=100), 'mlp, hidden_layers=(100)', X, y)
-
-
-
-# documents = zip(data, target)
-# nltk_classifier = NltkClassifier(documents)
-# nltk_classifier.test_classifier()
-
-# cnt_vectorizer = CountVectorizer(min_df=2)
-# cnt_vectorizer.fit_transform(X)
-# tf_vectorizer = TfidfVectorizer()
-
-# compare_classifiers(cnt_vectorizer, X, y, 'countvectorizer, min_df=2')
-# compare_classifiers(tf_vectorizer, X, y, 'tfidfvectorizer')
-
+evaluator = Evaluator(preprocessor=NLTKPreprocessor(add_PosNeg=True), vectorizer=tf_vectorizer, load_path='12grams')
+# params_grid = {'classifier:mlp__hidden_layer_sizes': [(30), (100)],
+#                'classifier:mlp__activation': ('logistic', 'tanh', 'relu'),
+#                'classifier:mlp__solver': ['lbfgs'],
+#                'classifier:mlp__alpha': [0.01, 0.001, 0.0001]
+#                }
+# evaluator.choose_classifier_parameters(MLPClassifier(), 'mlp', X, y, params_grid)
+evaluator.test_classifier(MLPClassifier(hidden_layer_sizes=(30), activation='logistic', solver='lbfgs', alpha=0.001), 'mlp', X, y, test_split=0)
 
 
 
